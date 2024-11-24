5e512d594a4f342093f74743d0d1dc11ce571ccc
springyu <75129303+yuspring@users.noreply.github.com>
2024-11-25 03:20:35 +0800

    更改HW2-1

--- HW2-1.s
+++ HW2-1.s
@@ -1,29 +1,33 @@
+.text
 main:
-	addi $s0, $zero, 5
+	addi $a0, $zero, 5
 	jal fact
 	j End
 	
 fact:
 	addi $sp, $sp, -8 
-	sw $ra, 0($sp)
-	sw $s0, 4($sp)
+	sw $ra, 4($sp)
+	sw $a0, 0($sp)
 	
-	slti $t0, $s0, 1
+	slti $t0, $a0, 1
 	beq $t0, $zero, L1
 	addi $v0, $zero, 1
 	addi $sp, $sp, 8
 	jr $ra
 
 L1:
-	addi $s0, $s0, -1
+	addi $a0, $a0, -1
 	jal fact
-	lw $s0, 4($sp)
+	lw $a0, 0($sp)
 	mul $v0, $a0, $v0
-	lw $ra, 0($sp)
+	lw $ra, 4($sp)
 	addi $sp, $sp, 8
 	jr $ra
 
 End:
+	move $a0, $v0
+	li $v0, 1
+	syscall
 	li $v0, 10
 	syscall
 
